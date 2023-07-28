497aea8f4a351437814a2d2f442bf2fa58b3b151
heyy-vivek <vivekupadhyay005@gmail.com>
2023-07-28 19:00:27 +0530

    final commit

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Book from './Book.gif'
+
+const Spinner=()=> {
+   
+    return (
+        <div className="text-center">
+      <img  src={Book} alt={Book} />
+      </div>
+    )
+  
+}
+
+export default Spinner
